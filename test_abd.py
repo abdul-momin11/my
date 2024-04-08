747584a5a7d5f84d1434180d944a2717b3a87ca1
abdul-momin11 <redhattesting04@gmail.com>
2024-04-08 08:39:08 +0500

    first commit

---
+++ test_abd.py
@@ -0,0 +1,29 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeDriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from allure_commons.types import AttachmentType
+def setup_function():
+    global driver
+    driver = webdriver.Chrome(service=ChromeDriver(ChromeDriverManager().install()))
+    driver.get('https://stage.alnafi.com/auth/signin')
+    driver.maximize_window()
+
+def teardown_function():
+    driver.quit()
+
+def my_cred():
+    return [
+        ('abdeali@gmail.com', 'abdeali@123'),
+        ('ali@gmail.com', 'ali@123')
+    ]
+@pytest.mark.parametrize("username,password",my_cred())
+def test_login(username,password):
+    print("My pytest login")
+    driver.find_element(By.ID,'Username/ Email').send_keys(username)
+    time.sleep(3)
+    driver.find_element(By.ID, 'Password').send_keys(password)
+    time.sleep(3)
